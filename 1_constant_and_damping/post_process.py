d54e7a9de32113f6af8af4f606561f7da131c2bd
yuan-feng <yuanfeng.energy@gmail.com>
2020-06-02 10:10:40 -0700

    polishing results

--- 1_constant_and_damping/post_process.py
+++ 1_constant_and_damping/post_process.py
@@ -1,6 +1,6 @@
 import torch
 import os
-from data import get_dataset, get_rawdata
+from data import get_dataset, get_rawdata, get_velo
 import matplotlib.pyplot as plt
 from nn_base import BaseNN
 from ecnn import ECNN
@@ -24,23 +24,6 @@ class ObjectView(object):
 args = ObjectView(args_dict)
 
 data = get_dataset()
-# plot config
-fig = plt.figure(figsize=(3, 3), facecolor='white')
-
-# Raw Dataset
-u, v, dudt, dvdt, t = get_rawdata()
-plt.scatter(u,v,c=t)
-plt.xlabel("$u$")
-plt.ylabel("$v$")
-plt.show()
-
-# with respect to t
-plt.plot(t,u,label='u')
-plt.plot(t,v,label='v')
-plt.xlabel("$t$")
-plt.ylabel("$u/v$")
-plt.show()
-
 
 # Construct Models
 def get_model(args, baseline):
@@ -72,65 +55,31 @@ kwargs = {'t_eval': np.linspace(t_span[0], t_span[1], 20000), 'rtol': 1e-12, 'da
 base_ivp = integrate_model( base_model, t_span, uv0, **kwargs )
 ecnn_ivp = integrate_model( ecnn_model, t_span, uv0, **kwargs )
 
-# test
-def forward(data):
-    test_uv = torch.tensor(data, requires_grad=True,dtype=torch.float32).view(1,2)
-    return ecnn_model.forward(test_uv).data
-data = [0.9, 0.2]
-print(forward(data))
-print(forward([v*0.9 for v in data]) )
-
-
-# Plot results
-fig = plt.figure()
-# Base
-LINE_SEGMENTS = 10
-for i, l in enumerate(np.split(base_ivp['y'].T, LINE_SEGMENTS)):
-    color = (float(i)/LINE_SEGMENTS, 0, 1-float(i)/LINE_SEGMENTS)
-    plt.plot(l[:,0],l[:,1],color=color)
-plt.title('Base NN')
-
-fig = plt.figure()
 LINE_SEGMENTS = 10
 base_v = []
 for i, l in enumerate(np.split(base_ivp['y'].T, LINE_SEGMENTS)):
-    color = (float(i)/LINE_SEGMENTS, 0, 1-float(i)/LINE_SEGMENTS)
-    tt = np.linspace(0, t_span[1], len(l[:,0])) + t_span[1] * i
     base_v = np.concatenate((base_v, l[:,0]))
-    plt.plot(tt,l[:,0],label='0')
-    plt.plot(tt,l[:,1],label='1')
-plt.title('Base NN')
-plt.show()
-
-
-# ECNN
-fig = plt.figure()
-LINE_SEGMENTS = 10
-for i, l in enumerate(np.split(ecnn_ivp['y'].T, LINE_SEGMENTS)):
-    color = (float(i)/LINE_SEGMENTS, 0, 1-float(i)/LINE_SEGMENTS)
-    plt.plot(l[:,0],l[:,1],color=color)
-plt.title('ECNN')
-
 
 fig = plt.figure()
-# print(base_ivp)
 LINE_SEGMENTS = 10
 ecnn_v = []
 for i, l in enumerate(np.split(ecnn_ivp['y'].T, LINE_SEGMENTS)):
     tt = np.linspace(0, t_span[1], len(l[:,0])) + t_span[1] * i
-    plt.plot(tt,l[:,0],label='0')
     ecnn_v = np.concatenate((ecnn_v, l[:,0]))
-#     plt.plot(tt,l[:,1],label='1')
 
-base_t = np.linspace(0, t_span[1]*LINE_SEGMENTS, len(base_v))
 
-plt.title('ECNN')
-plt.show()
-
+# groundtruth
+num_of_split = 1000
+base_t = np.linspace(0, t_span[1], len(base_v))
+u0 = 0
+v0 = base_v[0]
+omage = 0.1
+true_v = get_velo(u0, v0, base_t, omage)
 
 plt.title('comparison')
 plt.plot(base_t, base_v, label='base')
 plt.plot(base_t, ecnn_v, label='ecnn')
+plt.plot(base_t, true_v, label='groundtruth')
 plt.legend()
 plt.show()
 
