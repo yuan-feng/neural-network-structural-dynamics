d2dde93c46ea3074e97ad36b85cf1d84430da462
yuan-feng <yuanfeng.energy@gmail.com>
2020-06-02 18:42:00 -0700

    added pictures for constant

--- 1_constant_and_damping/post_process.py
+++ 1_constant_and_damping/post_process.py
@@ -1,6 +1,6 @@
 import torch
 import os
-from data import get_dataset, get_rawdata, get_velo
+from data import get_dataset, get_rawdata, get_velo, get_disp
 import matplotlib.pyplot as plt
 from nn_base import BaseNN
 from ecnn import ECNN
@@ -49,37 +49,81 @@ def integrate_model(model, t_span, uv0, **kwargs):
 # Analysis
 base_model = get_model(args, True)
 ecnn_model = get_model(args, False)
-t_span = [0,300]
+t_span = [0,30]
 uv0 = np.array([1.0, 0.])
-kwargs = {'t_eval': np.linspace(t_span[0], t_span[1], 20000), 'rtol': 1e-12, 'damping':0.02}
+kwargs = {'t_eval': np.linspace(t_span[0], t_span[1], 2000), 'rtol': 1e-12, 'damping':0.0}
 base_ivp = integrate_model( base_model, t_span, uv0, **kwargs )
 ecnn_ivp = integrate_model( ecnn_model, t_span, uv0, **kwargs )
 
 LINE_SEGMENTS = 10
-base_v = []
+base_dis = []
+base_vel = []
 for i, l in enumerate(np.split(base_ivp['y'].T, LINE_SEGMENTS)):
-    base_v = np.concatenate((base_v, l[:,0]))
+    base_dis = np.concatenate((base_dis, l[:,0]))
+    base_vel = np.concatenate((base_vel, l[:,1]))
 
 fig = plt.figure()
 LINE_SEGMENTS = 10
-ecnn_v = []
+ecnn_dis = []
+ecnn_vel = []
 for i, l in enumerate(np.split(ecnn_ivp['y'].T, LINE_SEGMENTS)):
-    tt = np.linspace(0, t_span[1], len(l[:,0])) + t_span[1] * i
-    ecnn_v = np.concatenate((ecnn_v, l[:,0]))
+    ecnn_dis = np.concatenate((ecnn_dis, l[:,0]))
+    ecnn_vel = np.concatenate((ecnn_vel, l[:,1]))
 
 
 # groundtruth
 num_of_split = 1000
-base_t = np.linspace(0, t_span[1], len(base_v))
-u0 = 0
-v0 = base_v[0]
-omage = 0.1
-true_v = get_velo(u0, v0, base_t, omage)
+base_t = np.linspace(0, t_span[1], len(base_dis))
+u0 = uv0[0]
+v0 = uv0[1]
+omage = 1
+# true_v = get_velo(u0, v0, base_t, omage)
+true_dis = get_disp(u0, v0, base_t, omage)
 
-plt.title('comparison')
-plt.plot(base_t, base_v, label='base')
-plt.plot(base_t, ecnn_v, label='ecnn')
-plt.plot(base_t, true_v, label='groundtruth')
-plt.legend()
+# #############################
+import matplotlib.pylab as pylab
+params = {'legend.fontsize': 'x-large',
+          # 'figure.figsize': (10, 8),
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
+pylab.rcParams.update(params)
+# #############################
+
+# #############################
+# #############################
+# plt.title('comparison')
+plt.plot(base_t, base_dis, color='b', label='Baseline Neural Network')
+plt.plot(base_t, ecnn_dis, color='r', label='Energy Constant Neural Network')
+plt.plot(base_t, true_dis, color='k', label='Groundtruth')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+          fancybox=True, shadow=True
+          # , ncol=3
+          )
+plt.xlabel(" Time [second] ")
+plt.ylabel(" Displacement [meters] ")
+plt.yticks(np.arange(true_dis.min(), true_dis.max()*1.01, 0.5))
+plt.grid()
+plt.savefig( "constant_comparison_disp.jpg",bbox_inches="tight")
+plt.show()
+
+
+# # #############################
+# # #############################
+energy_base_nn = base_dis**2 + base_vel**2
+energy_ecnn = ecnn_dis**2 + ecnn_vel**2
+
+plt.plot(base_t, energy_base_nn, color='b', label='Baseline Neural Network')
+plt.plot(base_t, energy_ecnn, color='r', label='Energy Constant Neural Network')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+          fancybox=True, shadow=True
+          # , ncol=3
+          )
+plt.xlabel(" Time [second] ")
+plt.ylabel(" System Energy")
+# plt.yticks(np.arange(energy_base_nn.min(), energy_base_nn.max()*1.01, 0.1))
+plt.grid()
+plt.savefig( "constant_comparison_error.jpg",bbox_inches="tight")
 plt.show()
 
